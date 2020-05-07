54a617861d5038f5530a12333e09df4b20100e5c
toleen98 <toleenballool98@gmail.com>
2020-05-07 18:24:18 +0300

    add cart alert

--- script.js
+++ script.js
@@ -9,15 +9,12 @@ function myItems(pic, name, price){
 }
 var array=["https://i.pinimg.com/564x/89/79/9b/89799beb3abb0130c4eb206fbdb1b44e.jpg,candy,2$","https://i.pinimg.com/474x/66/32/a5/6632a53a1e6a33c727ed73cf1825deea.jpg,candy,4$","https://i.pinimg.com/474x/07/c7/d3/07c7d35dd6dd6b93f8b0ec6f084be04c.jpg,candy,6$","https://i.pinimg.com/564x/05/94/63/059463624cac68a50303ad8e3c7672c0.jpg,candy,12$","https://i.pinimg.com/474x/2f/ae/35/2fae357bbabd8dce657cf1dde77e9b59.jpg,candy,7$"]
 
-function ourShopcandy(array){
-	var arr=[]
-array.forEach(function(item){
+function ourShopcandy(array){	
+return array.map(function(item){
 	var x=item.split(",")
-    var obj=myItems(x[0],x[1],x[2])
-    arr.push(obj)
+    return myItems(x[0],x[1],x[2])    
 })
-console.log(arr)
-return arr
+
 }
 //using jequery
 $("#loadmore").click(function MakeItem(){
@@ -36,9 +33,14 @@ $("#loadmore").click(function MakeItem(){
  	price.text(item.price);
  	price.appendTo(div)
  	var btn=$("<button>Add To Cart</button>")
+ 	btn.addClass("bton")
  	btn.appendTo(div)
 
  })
  
 
+})
+
+$(".bton").click(function addTocart() {
+   alert("item has been added")
 })
