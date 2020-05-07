c18722d7795fd93e5073babf84aba27c1a6f66fd
toleen98 <toleenballool98@gmail.com>
2020-05-07 11:07:39 +0300

    use jquery

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 
-function myItems
+function myItems(pic, name, price){
+	return {
+		pic: pic,
+		name: name,
+		price:price
 
-function addItem(item){
-	var div
+	}
+}
+var array=["https://i.pinimg.com/564x/05/94/63/059463624cac68a50303ad8e3c7672c0.jpg,candy,4$","https://i.pinimg.com/564x/05/94/63/059463624cac68a50303ad8e3c7672c0.jpg,candy,4$","https://i.pinimg.com/564x/05/94/63/059463624cac68a50303ad8e3c7672c0.jpg,candy,4$","https://i.pinimg.com/564x/05/94/63/059463624cac68a50303ad8e3c7672c0.jpg,candy,4$","https://i.pinimg.com/564x/05/94/63/059463624cac68a50303ad8e3c7672c0.jpg,candy,4$"]
 
+function addItem(array){
+	var arr=[]
+array.forEach(function(item){
+	var x=item.split(",")
+    var obj=myItems(x[0],x[1],x[2])
+    arr.push(obj)
+})
+console.log(arr)
+return arr
 }
